4a2eaaec987db5fece0963bb32b3a27b45659afe
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-04-08 11:05:23 -0600

    interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'scalffoldinAvanzado';
+export class AppComponent implements OnInit {
+  constructor(private readonly httpClient: HttpClient) {}
+  ngOnInit(): void {
+    this.httpClient
+      .get<any[]>('https://pokeapi.co/api/v2/pokemon/ditto')
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          console.log(`Se obtuvo ${res.length} resultados`);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
 }
--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -3,16 +3,24 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-
   constructor() {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error) => {
+        if (error.status === 404)
+          return throwError(() => 'No se encontro el URL');
+        return throwError(() => error.message);
+      })
+    );
   }
 }
